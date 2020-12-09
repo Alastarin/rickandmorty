f8288ffb652d5a524bc41e504e68ecac37969424
Alastarin <36680963+Alastarin@users.noreply.github.com>
2020-12-09 01:42:02 +0000

    Deploying to gh-pages from  @ 04d38463af9ef017e976aae204c1da692ffd4991 🚀

---
+++ _nuxt/static/1607478119/19/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fgh-pages\u002F_nuxt\u002Fstatic\u002F1607478119",layout:"default",error:a,state:{characters:{all:[],character:a,page:1,totalCount:0,noMore:false}},serverRendered:true,routePath:"\u002F19",config:{content:{dbHash:"fcef5b35"}}}}(null));
